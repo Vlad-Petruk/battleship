9e6859636d075851b0f70bea4e2407d05f7e28d9
Vlad-Petruk <vladpetruk00@gmail.com>
2024-04-24 13:16:07 +0300

    write function to mark space around succsessfull hit

--- src/factories.js
+++ src/factories.js
@@ -26,7 +26,7 @@ const Gameboard = () => {
         for (let i = 0; i <= 9; i++) {
           gameBoard[i] = [];
           for (let j = 0; j <= 9; j++) {
-            gameBoard[i][j] = null;
+            gameBoard[i][j] = '';
           }
         }
         return gameBoard;
@@ -45,10 +45,10 @@ const Gameboard = () => {
     //start and end here - indexes of the bord's cells where i want to place ship
     // the coordinate object should have a reference to the ship in its location.
     function placeShip(col, row, ship, direction) {
-        if (_isWithinBounds(col)&&_isWithinBounds(row)&&gameboard[col][row] === null) {
+        if (_isWithinBounds(col)&&_isWithinBounds(row)&&gameboard[col][row] === '') {
             if (direction === 'vertical'&&_isWithinBounds(col+ship.length)) {
                 for (let i = col; i < col+ship.length; i++) {
-                    if(gameboard[i][row] === null) {
+                    if(gameboard[i][row] === '') {
                     gameboard[i][row] = {
                         type: `${ship.length}ship`,
                         ship: ship,
@@ -60,7 +60,7 @@ const Gameboard = () => {
 
             if (direction === 'horizontal'&&_isWithinBounds(row+ship.length)) {
                 for (let i = row; i < row+ship.length; i++) {
-                    if(gameboard[col][i] === null) {
+                    if(gameboard[col][i] === '') {
                     gameboard[col][i] = {
                         type: `${ship.length}ship`,
                         ship: ship,
@@ -75,17 +75,16 @@ const Gameboard = () => {
     } 
 
     //Currently testing this in index.js
-    // Have an idea how to mark spots near wounded ship: to check coordinate of the spot with  if (col-1,col+1, row-1,row+1 all !== 'O') to become “·” U+00B7 Middle Dot Unicode Character.
     function receiveAttack (col, row) {
         if (_isWithinBounds(col)&&_isWithinBounds(row)) {
             let boardCell = gameboard[col][row];
-            if(boardCell === null) {
+            if(boardCell === '') {
                 gameboard[col][row] = '·';
                 console.log('Miss')
             } else if (boardCell.value === 'O') {
                 boardCell.ship.hit();
                 boardCell.value = 'X';
-                // _markCellsAraundSuccsessfullHit(col, row)
+                _markCellsAroundSuccsessfullHit(col, row)
                 console.log(boardCell.ship.getHits());
                 if(boardCell.ship.isSunk()) return console.log('Sunk')
             }
@@ -94,16 +93,53 @@ const Gameboard = () => {
         } else { return console.log('Attack out of board')}
     }
 
-    //Partly working but need to check corners neiborours also. And now only working with 1 deck ships(need to update cheking to somehow regulate out of board cells!!)
-    // function _markCellsAraundSuccsessfullHit(col, row) {
-    //     if(gameboard[col+1][row]===null&&gameboard[col-1][row]===null&&gameboard[col][row+1]===null&&gameboard[col][row-1]===null&&gameboard[col+1][row]===null&&gameboard[col-1][row]===null&&gameboard[col][row+1]===null&&gameboard[col][row-1]===null) {
-    //         gameboard[col+1][row] = '·';
-    //         gameboard[col-1][row] = '·';
-    //         gameboard[col][row+1] = '·';
-    //         gameboard[col][row-1] = '·';
-
-    //     }
-    // }
+    function _markCellsAroundSuccsessfullHit(col, row) {
+        const neighbours = [
+            gameboard[col + 1]?.[row], 
+            gameboard[col - 1]?.[row],
+            gameboard[col]?.[row + 1],
+            gameboard[col]?.[row - 1],
+            gameboard[col + 1]?.[row + 1],
+            gameboard[col - 1]?.[row - 1],
+            gameboard[col - 1]?.[row + 1],
+            gameboard[col + 1]?.[row - 1]
+        ];
+        
+        for (let i = 0; i < neighbours.length; i++) {
+            if (neighbours[i] !== undefined&&neighbours[i].value !== 'O'&&neighbours[i].value !== 'X') {
+                neighbours[i] = '·';
+                // Update corresponding value in gameboard array
+                switch (i) {
+                    case 0: 
+                        gameboard[col + 1][row] = '·';
+                        break;
+                    case 1: 
+                        gameboard[col - 1][row] = '·';
+                        break;
+                    case 2: 
+                        gameboard[col][row + 1] = '·';
+                        break;
+                    case 3:
+                        gameboard[col][row - 1] = '·';
+                        break;
+                    case 4: 
+                        gameboard[col + 1][row + 1] = '·';
+                        break;
+                    case 5: 
+                        gameboard[col - 1][row - 1] = '·';
+                        break;
+                    case 6: 
+                        gameboard[col - 1][row + 1] = '·';
+                        break;
+                    case 7:
+                        gameboard[col + 1][row - 1] = '·';
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
 
     return {
         gameboard,
